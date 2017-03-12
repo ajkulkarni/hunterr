98e3dbc2089dc0873159d23c84844edcc8398159
nakapika <aakulka9@asu.edu>
2017-03-12 04:41:49 -0700

    Google Sheets API

--- src/main/java/hunterr/helper/HunterrApiCalls.java
+++ src/main/java/hunterr/helper/HunterrApiCalls.java
@@ -1,28 +1,68 @@
 package hunterr.helper;
+
+import java.io.IOException;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.owlike.genson.Genson;
 
 public class HunterrApiCalls {
 	static Genson genson = new Genson();
-	
+	static Job[] jobs;
+
 	public static Job[] searchJobs(String city, String company, String title, String jobType, int days) {
 		Client client = ClientBuilder.newClient();
-		String response = client.target("http://api.indeed.com/ads")
-				.path("apisearch")
-				.queryParam("publisher", "2529823375038480")
-				.queryParam("l", city)
-				.queryParam("q", "company:" + company + " " + "title:" + title)
-				.queryParam("jt", jobType)
-				.queryParam("fromage", days)
-				.queryParam("v", "2")
-				.queryParam("format", "json")
-				.queryParam("limit", 100)
-				.request()
-				.get(String.class);
+		String response = client.target("http://api.indeed.com/ads").path("apisearch")
+				.queryParam("publisher", "2529823375038480").queryParam("l", city)
+				.queryParam("q", "company:" + company + " " + "title:" + title).queryParam("jt", jobType)
+				.queryParam("fromage", days).queryParam("v", "2").queryParam("format", "json").queryParam("limit", 100)
+				.request().get(String.class);
 		IndeedResponse iResponse = genson.deserialize(response, IndeedResponse.class);
-		return iResponse.getResults();
+		jobs = iResponse.getResults();
+		return jobs;
 	}
-}
 
+	public static void writeToSheet() throws IOException {
+		JSONArray jsonArray = new JSONArray();
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("range", "Sheet1!A1:F1");
+		jsonObject.put("majorDimension", "ROWS");
+		for (Job j : jobs) {
+			jsonArray.put(new JSONArray().put(j.getCity()).put(j.getCompany()).put(j.getFormattedRelativeTime())
+					.put(j.getJobtitle()).put(j.getState()).put(j.getUrl()));
+		}
+		jsonObject.put("values", jsonArray);
+		System.out.println(jsonObject.toString());
+		CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+		try {
+			HttpPost request = new HttpPost(
+					"https://sheets.googleapis.com/v4/spreadsheets/1JLzHOBy-JLkdWbDdwgUNKQDxkC5q7RC2MAw3M6GsPLU/values/Sheet1!A1:F1:append?valueInputOption=USER_ENTERED");
+			StringEntity params = new StringEntity(jsonObject.toString());
+			request.addHeader("content-type", "application/json");
+			request.addHeader("Authorization",
+					"Bearer ya29.GlsMBMO9CM6jf4I2y6The0eu6EuRCh0L2otHt9a9apMEdV1VdRayvA85-Pqky2n6BMSzR7dh_d2Aj8lLP69RpH_33z_q03erFSVkmrciz-TsZiCnsrftKc8ZIVig");
+			request.setEntity(params);
+			httpClient.execute(request);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			httpClient.close();
+		}
+
+	}
+
+	/*
+	 * public static void main(String args[]) throws IOException {
+	 * searchJobs("seattle", "microsoft", "software engineer", "fulltime", 30);
+	 * writeToSheet(); }
+	 */
+}
