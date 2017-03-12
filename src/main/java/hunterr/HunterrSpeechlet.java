54677050bd7dc6e2c6c20d14a64902cdc786377b
hmaskai <hmaskai@asu.edu>
2017-03-11 17:58:22 -0700

    Project Structure Setup

--- src/main/java/hunterr/HunterrSpeechlet.java
+++ src/main/java/hunterr/HunterrSpeechlet.java
@@ -1,5 +1,65 @@
 package hunterr;
 
-public class HunterrSpeechlet {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+
+import scorekeeper.ScoreKeeperSpeechlet;
+import scorekeeper.SkillContext;
+
+public class HunterrSpeechlet implements Speechlet{
+	 private static final Logger log = LoggerFactory.getLogger(ScoreKeeperSpeechlet.class);
+
+	    //private AmazonDynamoDBClient amazonDynamoDBClient;
+
+	    private HunterrResponseUtil responseUtil;
+
+	    private SkillContext skillContext;
+	    @Override
+		public void onSessionStarted(SessionStartedRequest request, Session session) throws SpeechletException {
+			// TODO Auto-generated method stub
+	        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
+	                session.getSessionId());
+
+	        //initializeComponents();
+
+	        // if user said a one shot command that triggered an intent event,
+	        // it will start a new session, and then we should avoid speaking too many words.
+	        skillContext.setNeedsMoreHelp(false);
+		}
+	    
+		@Override
+		public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public SpeechletResponse onLaunch(LaunchRequest request, Session session) throws SpeechletException {
+			// TODO Auto-generated method stub
+	        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
+	                session.getSessionId());
+
+	        skillContext.setNeedsMoreHelp(true);
+	        return responseUtil.getLaunchResponse(request, session);
+		}
+
+		@Override
+		public void onSessionEnded(SessionEndedRequest request, Session session) throws SpeechletException {
+			// TODO Auto-generated method stub
+	        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
+	                session.getSessionId());
+	        // any cleanup logic goes here
+		}
+		
+		
 
 }
--- src/main/java/hunterr/HunterrSpeechletRequestStreamHandler.java
+++ src/main/java/hunterr/HunterrSpeechletRequestStreamHandler.java
@@ -1,5 +1,33 @@
 package hunterr;
 
-public class HunterrSpeechletRequestStreamHandler {
+import java.util.HashSet;
+import java.util.Set;
 
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+
+/**
+ * This class could be the handler for an AWS Lambda function powering an Alexa
+ * Skills Kit experience. To do this, simply set the handler field in the AWS
+ * Lambda console to "scorekeeper.ScoreKeeperSpeechletRequestStreamHandler" For
+ * this to work, you'll also need to build this project using the
+ * {@code lambda-compile} Ant task and upload the resulting zip file to power
+ * your function.
+ */
+
+public class HunterrSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler{
+	private static final Set<String> supportedApplicationIds;
+
+	static {
+		/*
+		 * This Id can be found on https://developer.amazon.com/edw/home.html#/
+		 * "Edit" the relevant Alexa Skill and put the relevant Application Ids
+		 * in this Set.
+		 */
+		supportedApplicationIds = new HashSet<String>();
+		// supportedApplicationIds.add("amzn1.echo-sdk-ams.app.[unique-value-here]");
+	}
+
+	public HunterrSpeechletRequestStreamHandler() {
+        super(new HunterrSpeechlet(), supportedApplicationIds);
+    }
 }
